001762ce5b201be34d319c70d7a7ed82cfe11f62
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2014-11-28 20:44:46 +0000

    added comments

--- SecretSanta.py
+++ SecretSanta.py
@@ -1,6 +1,7 @@
 from random import shuffle
 
 def collect_users():
+    """Collect a list of all Santas from the user"""
     list_of_santas = []
     
     while 1:
@@ -13,6 +14,7 @@ def collect_users():
 
 
 def set_exceptions(list_of_santas):
+    """Set up any gifting exclusions that may exist"""
     print("Please set any matchup exclusions\n")
 
     exception_dict = {}
@@ -38,10 +40,10 @@ def set_exceptions(list_of_santas):
     return exception_dict
 
 def generate_matchups(exception_dict, list_of_santas):
+    """pseudorandomly assign giftees to gifters"""
     list1 = list(list_of_santas)
     list2 = list(list_of_santas)
     shuffle(list1)
-    shuffle(list2)
 
     a = list(zip(list1,list2))
     b = []
@@ -50,21 +52,25 @@ def generate_matchups(exception_dict, list_of_santas):
     return b
 
 def test_matchup(matchup,exception_dict):
+    """Test the proposed matchups agains the disallowed paths"""
     for i in range(len(exception_dict)):
         print(matchup[i][0] + "->" + matchup[i][1])
         if matchup[i][1] in exception_dict[matchup[i][0]]:
             return False
     return True
 
-
+#d is a test dictionary - collect_users and set_exceptions works, this is to speed up testing
 d = {'krish':['sav','krish'],'sav':['krish','sav'],'gabs':['seb','krish','gabs'],'seb':['gabs','seb'],'tim':['tim']}
 
-
+#######
+#this is a loop that could be used in the final program?
+#naively generate matchups until one is found that does not break the rules?
 valid = False
 while not valid:
     matchup = generate_matchups(d,"krish sav gabs seb tim".split(" "))
     print(matchup)
     valid = test_matchup(matchup,d)
-
+#end of potential real code
+######
 
 print("done")
