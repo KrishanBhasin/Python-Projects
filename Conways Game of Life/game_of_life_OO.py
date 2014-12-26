b9d3802186ddbdafd1fe5a4ce93704459dc0d2bc
Krishan Bhasin <krishan.bhasin42+github@gmail.com>
2014-12-26 18:31:31 +0300

    fixed GOL bugs! Code to be optimised and refactored, and more options added (eg rotation of glider)

--- Conways Game of Life/game_of_life_OO.py
+++ Conways Game of Life/game_of_life_OO.py
@@ -1,109 +1,131 @@
 from tkinter import *
-import numpy
+import time
 
 class game:
 	"""An object to store the game"""
-	
-	def __init__(self,size):
+
+	def __init__(self, size):
 		self.size = size
 		self.cell_size = 25
 		window = Tk()
-		self.canvas = Canvas(window, width = size*self.cell_size, height = size*self.cell_size)
-
+		self.canvas = Canvas(window, width=size * self.cell_size, height=size * self.cell_size)
 		self.canvas.pack()
-		
-		self.board = [[cell(i,j) for j in range(size)] for i in range(size)]
-		
+
+		self.board = [[cell(i, j) for i in range(size)] for j in range(size)]
+
 		return None
-	
+
 	def drawbox(self):
 		for j in range(self.size):
 			for i in range(self.size):
-				if self.board[j][i].alive == True:
-					self.canvas.create_rectangle(self.cell_size*i, self.cell_size*j, self.cell_size*i+self.cell_size, self.cell_size*j+self.cell_size, fill="#220C65", outline="#DFF2A6", width=1)
-				elif self.board[j][i].alive == False:
-					self.canvas.create_rectangle(self.cell_size*i, self.cell_size*j, self.cell_size*i+self.cell_size, self.cell_size*j+self.cell_size, fill="#B7F3D5", outline="#DFF2A6", width=1)
+				if self.board[i][j].alive:
+					self.box_colour = "#220C65"
+				else:
+					self.box_colour = "#B7F3D5"
+					
+				self.canvas.create_rectangle(self.cell_size * i, self.cell_size * j,self.cell_size * i + self.cell_size,self.cell_size * j + self.cell_size, fill=self.box_colour, outline="#FFFFFF",width=2)
+
 		return
+
+	def createGlider(self, x, y):
+		self.board[x - 1][y - 1].alive = True
+		self.board[x][y - 1].alive = True
+		self.board[x + 1][y - 1].alive = True
+		self.board[x + 1][y].alive = True
+		self.board[x][y + 1].alive = True
 		
-	def createGlider(self,y,x):
-		self.board[y-1][x-1].alive=True
-		self.board[y][x-1].alive=True
-		self.board[y+1][x-1].alive=True
-		self.board[y+1][x].alive=True
-		self.board[y][x+1].alive=True
-	
+	def createGun(self,x,y):
+		self.board
+
 
 class cell:
 	"""An object for the cells in Conway's Game of Life"""
-	def __init__(self,y,x):
+
+	def __init__(self, y, x):
 		self.x = x
 		self.y = y
 		self.alive = False
-		
-		
+
+	#self.num_of_neighbors = 0
+
 	def countNeighbors(self):
-		self.num_of_neighbors = -1
-		for j in [-1,0,1]:
-			for i in [-1,0,1]:
-				if (self.x+i)<0 or (self.y+i)<0:	#prevent negative indexing				#could move one of these out side the current for loop to be more efficient
-					print("preventing negative indexing...")
+		self.num_of_neighbors = 0
+		for j in [-1, 0, 1]:
+			for i in [-1, 0, 1]:
+				#print("Testing coordinate: " + str(self.x+i)+","+str(self.y+j))
+				if i == 0 and j == 0:
+					#print("Not counting self")
+					continue
+				elif (self.x + i) < 0 or (self.y + j < 0):
+					#print("Avoiding negative indexing")
 					continue
 				try:
-					if my_game.board[self.y +j][self.x +i].alive==True:		##THIS ISN'T CLEAN - USES PROGRAMMERS KNOWLEDGE OF THE BOARD OBJECT'S NAME
-						print("adding one")
-						self.num_of_neighbors+=1
+					if my_game.board[self.x + i][self.y + j].alive:
+						#print("adding 1 - ")
+						self.num_of_neighbors += 1
+						continue
 					else:
-						print("pass")
+						#print("passing over a blank square")
+
+						continue
 				except IndexError:
-					print("index error....")
+					#print("Index error caught - attempted to go out of bounds")
 					continue
-	
-	def cellLifeCheck(self):
-		if self.num_of_neighbors==2 or self.num_of_neighbors==3:
+
+
+	def livingCellCheck(self):
+		if self.num_of_neighbors in [2,3]:
 			self.alive = True
 		else:
-			cell.alive = False
-			
-	
-	def cellBornCheck(self):
+			self.alive = False
+
+
+	def deadCellCheck(self):
 		if self.num_of_neighbors == 3:
 			self.alive = True
 		else:
 			self.alive = False
-			
+
 ######################################################################################
 
 
+#TODO - automate the generation cyling
+#TODO - generate more templates (gun, oscillator,LWSS etc)
+#TODO - 
+
+
 num_of_generations = int(input("How many generations do you wish to simulate?\n"))
-			
-my_game = game(5)
+
+my_game = game(20)
 
 my_game.drawbox()
 
-my_game.createGlider(2,2)
+my_game.board[1][1].alive = True
+my_game.board[1][2].alive = True
+my_game.board[2][2].alive = True
+
+
+my_game.createGlider(5, 10)
+my_game.drawbox()
 
 for _ in range(num_of_generations):
-	
 	for a in range(my_game.size):
 		for b in range(my_game.size):
-			my_game.board[b][a].countNeighbors()
-			print(my_game.board[b][a].x,my_game.board[b][a].y)
-			print(my_game.board[b][a].num_of_neighbors)
-			print("*x*x*")
-			
-	
-	
+			my_game.board[a][b].countNeighbors()
+			#print(my_game.board[a][b].x, my_game.board[a][b].y)
+			#print("number of neighbors: " + str(my_game.board[a][b].num_of_neighbors))
+			#print(my_game.board[a][b].alive)
+			#print("*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*x*")
+
 	for a in range(my_game.size):
 		for b in range(my_game.size):
 			#print(my_game.board[a][b].num_of_neighbors)
-			if my_game.board[b][a].alive:
-				my_game.board[b][a].cellLifeCheck()
+			if my_game.board[a][b].alive:
+				my_game.board[a][b].livingCellCheck()
 			else:
-				my_game.board[b][a].cellBornCheck()
-			
-	
-	my_game.drawbox()
+				my_game.board[a][b].deadCellCheck()
 	input()
 	
+	my_game.drawbox()
 
-	
+
